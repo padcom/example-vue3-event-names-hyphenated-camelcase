13d5eba985c1b9776b853c1bb9fcc253a1ecbe6e
Matthias Hryniszak <padcom@gmail.com>
2024-01-15 10:46:32 +0100

    Initial import

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello, world! {{ message }}</h1>
+  <button @click="$emit('helloWorld')">Say hello</button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  message: { type: String, default: 'from HelloWorld.vue' },
+})
+
+defineEmits(['helloWorld'])
+</script>
+
+<style lang="postcss" scoped>
+h1 {
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+}
+</style>
